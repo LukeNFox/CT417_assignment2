52bfe5f6871f534fbf127ffd6f16837a7d6758c4
Luke Fox <l.fox9@nuigalway.ie>
2019-11-17 18:04:55 +0000

    adding tests for creation and updating of student class

--- src/test/java/nuigalway/app/StudentTest.java
+++ src/test/java/nuigalway/app/StudentTest.java
@@ -1,22 +1,63 @@
 package nuigalway.app;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import org.joda.time.LocalDate;
 import org.junit.Test;
+import java.util.ArrayList;
+import static org.junit.Assert.*;
 
 public class StudentTest
 {
 
-    @Test
-    public void testUsername()
-    {
-        Student luke = new Student("Luke", new LocalDate(1997,10,15));
+    private LocalDate dob = new LocalDate(1997,10,15);
+    private int age = 22;
+    private String name = "Luke";
+    private String username = "Luke22";
+    private Student student = new Student(name, dob);
 
-        assertTrue(luke.getUsername().equals("Luke22"));
-        assertFalse(luke.getUsername().equals("Luke76"));
+    private String newName = "Tom";
+    private String newUsername = "Tom2";
+    private int newAge = 2;
+    private LocalDate newDOB = new LocalDate(2017,10,15);
+
+    private CourseProgramme course = null;
+    private ArrayList<Module> module = null;
+
+    @Test
+    public void testUsername() {
+        assertTrue(student.getUsername().equals(username));
     }
 
+    @Test
+    public void testAge(){
+        assertTrue(student.getAge() == age);
+    }
+
+    @Test
+    public void testStudentCreation(){
+        assertTrue(student.getName().equals(name));
+        assertTrue(student.getDob().equals(dob));
+        assertTrue(student.getCourse() == null);
+        assertTrue(student.getModules() == null);
+    }
+
+    @Test
+    public void testStudentAlterations(){
+        student.setName(newName);
+        assertEquals(student.getName(),newName);
+        student.setDob(newDOB);
+        assertEquals(student.getAge(), newAge);
+        assertEquals(student.getDob(),newDOB);
+        assertEquals(student.getUsername(),newUsername);
+    }
+
+    @Test
+    public void testModule(){
+
+    }
+
+    @Test
+    public void testCourse(){
 
 
+    }
 }
